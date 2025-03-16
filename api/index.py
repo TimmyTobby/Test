f45f50a1335f6c66b4c48765dd1e447254bdfb4d
Timilehin <fajututimilehin@gmail.com>
2025-03-16 08:21:25 +0100

    updated deployment files

--- api/index.py
+++ api/index.py
@@ -6,7 +6,6 @@ app = FastAPI()
 def read_root():
     return {"message": "Deployment on Vercel successful!"}
 
-# Additional route for testing
 @app.get("/status")
 def status():
     return {"status": "App is running on Vercel!"}
