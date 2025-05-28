d66c1db2ef1a4d8895a315452c4434a07ec8b61f
Timilehin <fajututimilehin@gmail.com>
2025-05-28 17:14:32 +0100

    testing railway deployment V4

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Deployment on Railway successful! This is Version 4, Testing the V3 now!!!"}
+    return {"message": "Deployment on Railway successful! This is Version 4, Testing the V4 now!!!, running on Railway!"}
 
 @app.get("/status")
 def status():
